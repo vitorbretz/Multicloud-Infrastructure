c425d597a5d5195154ef90fe1d1e4ce1869a0ff9
vitorbretz <vitorbretz28@gmail.com>
2026-07-16 00:04:11 -0300

    fix

---
+++ main.tf
@@ -0,0 +1,14 @@
+# AWS provider
+provider "aws" {
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+  region     = "us-east-1"
+}
+​
+# Azure provider
+provider "azurerm" {
+  client_id = var.azure_client_id
+  client_secret = var.azure_client_secret
+  subscription_id = var.azure_subscription_id
+  tenant_id = var.azure_tenant_id
+}
---
+++ variables.tf
@@ -0,0 +1,9 @@
+# AWS credentials
+variable "aws_access_key" {}
+variable "aws_secret_key" {}
+
+# Azure credentials
+variable "azure_client_id" {}
+variable "azure_client_secret" {}
+variable "azure_subscription_id" {}
+variable "azure_tenant_id" {}
