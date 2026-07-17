04e2814ce249d13ec9b763d2396d823bac3b8a30
vitorbretz <vitorbretz28@gmail.com>
2026-07-16 23:08:40 -0300

    configuration storage in azure

--- main.tf
+++ main.tf
@@ -96,3 +96,25 @@ resource "aws_s3_bucket_policy" "bucket_policy" {
     ]
   })
 }
+
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-static-website"
+  location = "East US"
+}
+
+resource "azurerm_storage_account" "storage" {
+  name                     = "myaccounttostorageweb"
+  resource_group_name      = azurerm_resource_group.rg.name
+  location                 = azurerm_resource_group.rg.location
+
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "StorageV2"
+}
+
+resource "azurerm_storage_account_static_website" "website" {
+  storage_account_id = azurerm_storage_account.storage.id
+
+  index_document = "index.html"
+  error_404_document = "error.html"
+}
