edf1ff3528805e83007737e4e04f6059d558fecc
vitorbretz <vitorbretz28@gmail.com>
2026-07-16 21:34:30 -0300

    feat: configure s3 bucket

--- main.tf
+++ main.tf
@@ -12,3 +12,79 @@ provider "azurerm" {
   subscription_id = var.azure_subscription_id
   tenant_id       = var.azure_tenant_id
 }
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.weather_app.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "public_access" {
+  bucket                  = aws_s3_bucket.weather_app.id
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
+# Upload website files to the S3 bucket
+resource "aws_s3_object" "website_index" {
+  bucket = aws_s3_bucket.weather_app.id
+  key    = "index.html"
+  source = "website/index.html"
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "website_style" {
+  bucket = aws_s3_bucket.weather_app.id
+  key    = "styles.css"
+  source = "website/styles.css"
+  content_type = "text/css"
+}
+
+resource "aws_s3_object" "website_script" {
+  bucket = aws_s3_bucket.weather_app.id
+  key    = "script.js"
+  source = "website/script.js"
+  content_type = "application/javascript"
+}
+
+# Upload assets (images) to the S3 bucket
+resource "aws_s3_object" "website_assets" {
+  for_each = fileset("website/assets", "*")
+  bucket   = aws_s3_bucket.weather_app.id
+  key      = "assets/${each.value}"
+  source   = "website/assets/${each.value}"
+}
+
+# Add a bucket policy to allow public read access
+resource "aws_s3_bucket_policy" "bucket_policy" {
+  bucket = aws_s3_bucket.weather_app.id
+  policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Sid       = "PublicReadGetObject",
+        Effect    = "Allow",
+        Principal = "*",
+        Action    = "s3:GetObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.weather_app.id}/*"
+      },
+      {
+        Sid       = "CloudFrontLogsWrite",
+        Effect    = "Allow",
+        Principal = {
+          Service = "cloudfront.amazonaws.com"
+        },
+        Action    = "s3:PutObject",
+        Resource  = "arn:aws:s3:::${aws_s3_bucket.weather_app.id}/cloudfront-logs/*"
+      }
+    ]
+  })
+}
