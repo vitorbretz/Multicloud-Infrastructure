be0a0c365023995a2a7bcad3621846fdc572490f
vitorbretz <vitorbretz28@gmail.com>
2026-08-06 23:35:02 -0300

    fix

--- s3-bucket.tf
+++ s3-bucket.tf
@@ -9,9 +9,9 @@ resource "aws_s3_bucket" "this" {
     }
   )
 
-  lifecycle {
-    prevent_destroy = true
-  }
+  # lifecycle {
+  #   prevent_destroy = true
+  # }
 }
 
 resource "aws_s3_bucket_public_access_block" "this" {
